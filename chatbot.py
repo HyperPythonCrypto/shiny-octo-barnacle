73946018fab942dedd03cd21c63b8871b55f7dc2
HyperPythonCrypto <adityanowlakha@gmail.com>
2024-06-28 18:06:38 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,16 @@
+from ChatterBot import ChatBot
+from chatterbot.trainers import ListTrainer
 
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+
+trainer.train(conversation)
